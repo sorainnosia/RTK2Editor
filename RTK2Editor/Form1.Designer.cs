ec49fe86fbaf5a39f0a44578d18439cd9845fd54
sorainnosia <sorainnosia@gmail.com>
2020-09-12 01:39:28 +0800

    Add files via upload

---
+++ RTK2Editor/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace RTK2Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbReinforce = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtbRuin = new System.Windows.Forms.RichTextBox();
+            this.rtbProsperity = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rtbReinforce
+            // 
+            this.rtbReinforce.Location = new System.Drawing.Point(12, 85);
+            this.rtbReinforce.Name = "rtbReinforce";
+            this.rtbReinforce.Size = new System.Drawing.Size(247, 314);
+            this.rtbReinforce.TabIndex = 0;
+            this.rtbReinforce.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(604, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(622, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(696, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Reinforce Army";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ruin Army";
+            // 
+            // rtbRuin
+            // 
+            this.rtbRuin.Location = new System.Drawing.Point(265, 85);
+            this.rtbRuin.Name = "rtbRuin";
+            this.rtbRuin.Size = new System.Drawing.Size(247, 314);
+            this.rtbRuin.TabIndex = 5;
+            this.rtbRuin.Text = "";
+            // 
+            // rtbProsperity
+            // 
+            this.rtbProsperity.Location = new System.Drawing.Point(517, 85);
+            this.rtbProsperity.Name = "rtbProsperity";
+            this.rtbProsperity.Size = new System.Drawing.Size(247, 314);
+            this.rtbProsperity.TabIndex = 7;
+            this.rtbProsperity.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(513, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Prosperity";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(382, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(62, 26);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(450, 56);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(62, 26);
+            this.textBox3.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 406);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rtbProsperity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rtbRuin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rtbReinforce);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbReinforce;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtbRuin;
+        private System.Windows.Forms.RichTextBox rtbProsperity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ RTK2Editor/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RTK2Editor
+{
+    public partial class Form1 : Form
+    {
+        public List<byte> Datas = new List<byte>();
+        public Dictionary<int, string> Addr = new Dictionary<int, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadSetting();
+            if (string.IsNullOrEmpty(textBox1.Text) && File.Exists(textBox1.Text)) button1_Click(button1, EventArgs.Empty);
+            LoadAddress();
+        }
+
+        private void LoadAddress()
+        {
+            int a = 11668;
+            for (int i=0; i<49; i++)
+            {
+                Addr.Add(i + 1, (a + (i * 35)).ToString("X"));
+            }
+            //Addr.Add(3, "2dda");
+            //Addr.Add(4, "2dfd");
+            //Addr.Add(5, "2e20");
+            //Addr.Add(7, "2e66");
+            //Addr.Add(9, "2eac");
+            //Addr.Add(10, "2ecf");
+            //Addr.Add(11, "2ef2");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Input a filename");
+                return;
+            }
+            if (File.Exists(textBox1.Text) == false)
+            {
+                MessageBox.Show("File not found");
+                return;
+            }
+            Datas = File.ReadAllBytes(textBox1.Text).ToList();
+        }
+
+        private void SaveSetting()
+        {
+            File.WriteAllText("filename.txt", textBox1.Text);
+            File.WriteAllText("reinforce.txt", rtbReinforce.Text);
+            File.WriteAllText("ruin.txt", rtbRuin.Text);
+            File.WriteAllText("ruin1.txt", textBox2.Text);
+            File.WriteAllText("ruin2.txt", textBox3.Text);
+            File.WriteAllText("prosperity.txt", rtbProsperity.Text);
+        }
+
+        private void LoadSetting()
+        {
+            if (File.Exists("filename.txt")) textBox1.Text = File.ReadAllText("filename.txt");
+            if (File.Exists("reinforce.txt")) rtbReinforce.Text = File.ReadAllText("reinforce.txt");
+            if (File.Exists("ruin.txt")) rtbRuin.Text = File.ReadAllText("ruin.txt");
+            if (File.Exists("prosperity.txt")) rtbProsperity.Text = File.ReadAllText("prosperity.txt");
+            if (File.Exists("ruin1.txt")) textBox2.Text = File.ReadAllText("ruin1.txt");
+            if (File.Exists("ruin2.txt")) textBox3.Text = File.ReadAllText("ruin2.txt");
+        }
+
+        private int FindIndex(string str)
+        {
+            for (int i=0; i<Datas.Count; i++)
+            {
+                int count = 0;
+                for (int j=0;j<str.Length; j++)
+                {
+                    int ni = i + j;
+                    if (ni >= Datas.Count) break;
+                    if ((Datas[ni] >= 97 && Datas[ni] <= 122) || (Datas[ni] >= 65 && Datas[ni] <= 90) || (Datas[ni] == 32))
+                    {
+                        if (((char)Datas[ni]).ToString().ToUpper() == ((char)str[j]).ToString().ToUpper())
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (count == str.Length)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void ApplySetting()
+        {
+            if (string.IsNullOrEmpty(rtbReinforce.Text) == false)
+            {
+                List<string> reinforces = rtbReinforce.Text.Split(new char[] { '\n' }).ToList();
+                foreach (string reinforce in reinforces)
+                {
+                    string str = reinforce.Trim();
+                    if (string.IsNullOrEmpty(str)) continue;
+                    int index = FindIndex(str);
+                    if (index <= -1) continue;
+                    int first3 = index - 24;
+                    Datas[first3] = 255;
+                    Datas[first3 + 1] = 255;
+                    Datas[first3 + 2] = 255;
+                    Datas[first3 + 5] = 255;
+                    Datas[first3 + 7] = 255; // loyalty
+
+                    int last2 = index - 10; // army
+                    if (last2 <= -1) continue;
+                    Datas[last2] = 16;
+                    Datas[last2 + 1] = 39;
+                }
+            }
+
+            if (string.IsNullOrEmpty(rtbRuin.Text) == false)
+            {
+                int t1 = 0;
+                int.TryParse(textBox2.Text, out t1);
+                int t2 = 0;
+                int.TryParse(textBox3.Text, out t2);
+                if (t1 == 0) t1 = 1;
+                if (t1 > 255) t1 = 255;
+                if (t2 > 255) t2 = 255;
+                if (t1 < 0) t1 = 0;
+                if (t2 < 0) t2 = 0;
+                List<string> ruins = rtbRuin.Text.Split(new char[] { '\n' }).ToList();
+                foreach (string ruin in ruins)
+                {
+                    string str = ruin.Trim();
+                    if (string.IsNullOrEmpty(str)) continue;
+                    int index = FindIndex(str);
+                    if (index <= -1) continue;
+
+                    int last2 = index - 10; // army
+                    if (last2 <= -1) continue;
+                    Datas[last2] = (byte)t1;
+                    Datas[last2 + 1] = (byte)t2;
+                }
+            }
+
+            if (string.IsNullOrEmpty(rtbProsperity.Text) == false)
+            {
+                List<string> nos = rtbProsperity.Text.Split(new char[] { '\n' }).ToList();
+                foreach(string no in nos)
+                {
+                    if (string.IsNullOrEmpty(no)) continue;
+                    int n = 0;
+                    int.TryParse(no, out n);
+                    if (n == 0) continue;
+                    if (Addr.ContainsKey(n) == false) continue;
+                    string adr = Addr[n];
+                    int loc = Convert.ToInt32(adr, 16);
+                    Datas[loc] = 255;
+                    Datas[loc+1] = 255;
+                    Datas[loc+2] = 255;
+                    Datas[loc+3] = 255;
+                    Datas[loc+4] = 4;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveSetting();
+            if (Datas.Count == 0)
+            {
+                MessageBox.Show("Load file first");
+                return;
+            }
+            ApplySetting();
+            File.WriteAllBytes(textBox1.Text, Datas.ToArray());
+            MessageBox.Show("Successful");
+        }
+    }
+}
